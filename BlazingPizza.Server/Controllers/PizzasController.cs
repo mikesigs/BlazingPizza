1efc3fb26392df4faac149ddb7436a1c0ab89e11
Mike Sigsworth <mikesigsworth@gmail.com>
2021-09-13 22:24:49 -0600

    All

---
+++ BlazingPizza.Server/Controllers/PizzasController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BlazingPizza.Server.Data;
+
+namespace BlazingPizza.Server.Controllers
+{
+    [Route("pizzas")]
+    [ApiController]
+    public class PizzasController : Controller
+    {
+        private readonly PizzaStoreContext _db;
+
+        public PizzasController(PizzaStoreContext db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ BlazingPizza.Server/Model/PizzaStoreUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlazingPizza.Server.Model
+{
+    public class PizzaStoreUser : IdentityUser
+    {
+    }
+}
